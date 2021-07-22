519496001080f01a6b783518de0eab2600d286ea
A-Kija <arvydas.kijakauskas@gmail.com>
2021-07-22 15:06:52 +0300

    Pirmas

---
+++ 01/kitas.php
@@ -0,0 +1,28 @@
+<?php
+
+
+echo '<h1>Viso gero</h1>';
+
+
+
+
+$labas = 2;
+
+
+echo $labas++ * ++$labas;
+// 2 * 4
+
+
+// echo ++$labas;
+echo '<br>';
+// echo $labas;
+
+
+$pirmas = 'bla bla';
+$antras = 'ku kū';
+$trecias = $pirmas . $antras;
+
+
+echo $trecias;
+
+
